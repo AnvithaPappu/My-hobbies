edb5ba8d334042e0dece1e67ded8e3f738305566
AnvithaPappu <79202268+AnvithaPappu@users.noreply.github.com>
2021-02-17 17:37:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function changeimg(){
+  document.getElementById("imgdance").src="https://images.unsplash.com/photo-1610456274576-ffd5777e44ea?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85";
+
+}
